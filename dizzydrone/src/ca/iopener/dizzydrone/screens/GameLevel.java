954b73089360c7d2c135bbc002653a95f9b66736
mrprowse <jsprowse@cbe.ab.ca>
2013-05-21 23:02:18 -0600

    Resets due to non-fast forward Git error

--- dizzydrone/src/ca/iopener/dizzydrone/screens/GameLevel.java
+++ dizzydrone/src/ca/iopener/dizzydrone/screens/GameLevel.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Array;
@@ -61,26 +62,22 @@ public class GameLevel extends InputAdapter implements Screen {
 
 		if ((dizzy.status & Dizzy.PAUSED) > 0) {
 			if (Gdx.input.justTouched()) {
-				// Move Dizzy if paused and user clicks
-				dizzy.status = dizzy.status & ~Dizzy.PAUSED;
+				// Move Dizzy if paused an user clicks
+				dizzy.status = dizzy.status & Dizzy.MOVING & ~Dizzy.PAUSED;
 			}
 		} else {
 			if (Gdx.input.isTouched()) {
-				if (!isTouched) {
-					Gdx.app.log("INFO", "touched");
-					isTouched = true;
-					// Don't move Dizzy while player is touching screen
-					dizzy.status = dizzy.status & ~Dizzy.MOVING;
-				}
+				Gdx.app.log("INFO", "touched");
+				isTouched = true;
+				// Don't move Dizzy while player is touching screen
+				dizzy.status = dizzy.status & ~Dizzy.MOVING;
 			} else if (isTouched) {
 				// This is the case where the touch has just ended
 				Gdx.app.log("INFO", "touch over");
 				isTouched = false;
-				Gdx.app.log("INFO",
-						"Before: " + Float.toString(dizzy.vector.angle()));
+				Gdx.app.log("INFO", "Before: " + Float.toString(dizzy.vector.angle()));
 				dizzy.vector.setAngle(dizzy.getRotation());
-				Gdx.app.log("INFO",
-						"After: " + Float.toString(dizzy.vector.angle()));
+				Gdx.app.log("INFO", "After: " + Float.toString(dizzy.vector.angle()));
 				dizzy.status = dizzy.status | Dizzy.MOVING;
 			} else {
 				// Default is Dizzy spins and moves
@@ -89,15 +86,15 @@ public class GameLevel extends InputAdapter implements Screen {
 		}
 		// Move Dizzy
 		if ((dizzy.status & Dizzy.MOVING) > 0) {
-			bounceDizzy();
 			dizzy.move();
 		}
 		if ((dizzy.status & Dizzy.SPINNING) > 0) {
 			dizzy.spin();
 		}
+		bounceDizzy();
 
 		fps.log();
-		// Gdx.app.log("INFO", dizzy.toString());
+		//Gdx.app.log("INFO", dizzy.toString());
 
 		stage.act(delta);
 		stage.draw();
@@ -105,9 +102,8 @@ public class GameLevel extends InputAdapter implements Screen {
 		batch.begin();
 		// font.draw(batch, Float.toString(dizzy.getRotation()) + " @ "
 		// + dizzy.position.len(), dizzy.getX(), dizzy.getY() - 20);
-		font.draw(batch, dizzy.vector + " @ " + dizzy.vector.angle() + "/"
-				+ dizzy.getRotation(), dizzy.getX(),
-				dizzy.getY() + (2 * dizzy.getHeight()));
+		font.draw(batch, dizzy.vector + " @ " + dizzy.vector.angle() +  "/" + dizzy.getRotation(),
+				dizzy.getX(), dizzy.getY() + (2 * dizzy.getHeight()));
 		batch.end();
 	}
 
@@ -119,7 +115,7 @@ public class GameLevel extends InputAdapter implements Screen {
 				Gdx.app.log("INFO",
 						"intersect at " + a.getName() + " @ " + a.getX() + "/"
 								+ a.getY());
-				dizzy.vector.scl(1.2f).clamp(1f, 5f);
+				dizzy.vector.set(-dizzy.vector.x, -dizzy.vector.y);
 				a.setVisible(false);
 				a.remove();
 			}
@@ -129,7 +125,7 @@ public class GameLevel extends InputAdapter implements Screen {
 		if (dizzy.getX() < 0
 				|| (dizzy.getX() + dizzy.getWidth()) > stage.getWidth()) {
 			dizzy.vector.x *= -1;
-			dizzy.vector.scl(0.95f).clamp(1f, 5f);
+			// dizzy.vector.scl(1.1f).limit(5);
 			if (dizzy.getX() < 0) {
 				dizzy.setX(0);
 			} else {
@@ -139,7 +135,7 @@ public class GameLevel extends InputAdapter implements Screen {
 		if (dizzy.getY() < 0
 				|| (dizzy.getY() + dizzy.getHeight()) > stage.getHeight()) {
 			dizzy.vector.y *= -1;
-			dizzy.vector.scl(0.95f).clamp(1f, 5f);
+			// dizzy.vector.scl(1.1f).limit(5);
 			if (dizzy.getY() < 0) {
 				dizzy.setY(0);
 			} else {
@@ -170,8 +166,8 @@ public class GameLevel extends InputAdapter implements Screen {
 				actor.setPosition(stage.getWidth() / 2 - dizzy.getWidth() / 2,
 						stage.getHeight() / 2 - dizzy.getHeight() / 2);
 			} else {
-				actor.setPosition((float) Math.random() * (stage.getWidth() - (2 * actor.getWidth())) + actor.getWidth(),
-						(float) Math.random() * (stage.getHeight() - (2 * actor.getHeight())) + actor.getHeight());
+				actor.setPosition((float) Math.random() * stage.getWidth(),
+						(float) Math.random() * stage.getHeight());
 			}
 		}
 	}
